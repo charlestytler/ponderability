9cc3d3126d63c6b097b70269c3eec4c2ea6f37bc
Charles Tytler <cjtytler@gmail.com>
2021-12-05 23:07:56 -0500

    Example plot

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import "./Page.css";
+import ExamplePlot from "../plots/ExamplePlot";
 
 function HomePage() {
   return (
     <div>
       <header className="Page">
         <h2>Home</h2>
+        <ExamplePlot />
       </header>
     </div>
   );
---
+++ src/plots/ExamplePlot.js
@@ -0,0 +1,33 @@
+import { LineChart, Line } from "recharts";
+
+function f(x) {
+  return 1.0 - Math.exp(-0.707 * x);
+}
+
+function quad(x) {
+  return 0.08 * Math.pow(x - 5, 2);
+}
+
+function cubic(x) {
+  return 0.8 * Math.sin(16 * x) + 0.7;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const data = array.map((x) => ({
+  t: x,
+  f: f(x),
+  quad: quad(x),
+  cubic: cubic(x),
+}));
+
+function ExamplePlot() {
+  return (
+    <LineChart width={800} height={600} data={data}>
+      <Line type="monotone" dataKey="f" stroke="#8884d8" />
+      <Line type="monotone" dataKey="quad" stroke="#8884d8" />
+      <Line type="monotone" dataKey="cubic" stroke="#8884d8" />
+    </LineChart>
+  );
+}
+
+export default ExamplePlot;
